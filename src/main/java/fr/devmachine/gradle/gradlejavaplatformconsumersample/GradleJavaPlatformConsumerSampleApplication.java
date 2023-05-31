977b0b623304827b13789dbfca989fb292afcadd
Olivier Boudet <olivier.boudet@cooperl.com>
2023-05-31 23:06:40 +0200

    init

---
+++ src/main/java/fr/devmachine/gradle/gradlejavaplatformconsumersample/GradleJavaPlatformConsumerSampleApplication.java
@@ -0,0 +1,13 @@
+package fr.devmachine.gradle.gradlejavaplatformconsumersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJavaPlatformConsumerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJavaPlatformConsumerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/devmachine/gradle/gradlejavaplatformconsumersample/GradleJavaPlatformConsumerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devmachine.gradle.gradlejavaplatformconsumersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleJavaPlatformConsumerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
